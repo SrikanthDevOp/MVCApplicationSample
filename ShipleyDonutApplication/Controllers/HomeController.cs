1ddf5b5121ed0f8d81f79d7dc9471f4fcdc5075d
Srikanth Paladugula <spaladugula@microsoft.com>
2022-04-11 23:09:06 +0530

    Added views.

--- ShipleyDonutApplication/Controllers/HomeController.cs
+++ ShipleyDonutApplication/Controllers/HomeController.cs
@@ -20,9 +20,42 @@ namespace ShipleyDonutApplication.Controllers
             return View();
         }
 
+        public ActionResult Menu()
+        {
+            ViewBag.Message = "Your menu page.";
+
+            return View();
+        }
+
+        public ActionResult Franchise()
+        {
+            ViewBag.Message = "Your franchise page.";
+
+            return View();
+        }
+
+        public ActionResult Community()
+        {
+            ViewBag.Message = "Your community page.";
+
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            ViewBag.Message = "Your shop page.";
+
+            return View();
+        }
+        public ActionResult StoreLocator()
+        {
+            ViewBag.Message = "Your store locator page.";
 
             return View();
         }
